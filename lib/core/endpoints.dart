0ca206bf76dba98b5267dcaa7b883c91b81b213d
charan <saicharan.teamup@gmail.com>
2025-11-25 20:00:17 +0530

    committed by charan 25/11/2025 at 8:00 PM

--- lib/core/endpoints.dart
+++ lib/core/endpoints.dart
@@ -4,7 +4,7 @@ class APIEndpointUrls {
   static const String ApiUrl = '${ApiConfig.baseUrl}';
   static const String AuthUrl = '${ApiUrl}parentauth/';
   static const String story = '${ApiUrl}story/';
-
+  static const String children='${ApiUrl}children';
 
   /// Auth URls
   static const String login = '${AuthUrl}login';
@@ -12,7 +12,7 @@ class APIEndpointUrls {
 
   static const String fetchCategory='${story}fetchcategories';
   static const String fetchSubCategory='${story}fetchsubcategoriesbycategoryid/';
-  static const String children='${ApiUrl}children';
   static const String getAllChildren='${children}/getallchildrensofaparent';
+  static const String createChildren='${children}/createchild';
 
 }
