db36468cc619d817e740c8dc8222c498911a5514
charan <saicharan.teamup@gmail.com>
2025-11-25 12:00:56 +0530

    committed by charan 25/11/2025 at 11:59 Am

--- lib/app_routes/app_pages.dart
+++ lib/app_routes/app_pages.dart
@@ -1,8 +1,22 @@
 import 'package:get/get.dart';
 import 'package:story_hug/pages/profile.dart';
 import 'package:story_hug/pages/sign_up_screen.dart';
-
+import '../pages/Passwordchange/passwordchange.dart';
+import '../pages/Reminder/reminders.dart';
+import '../pages/Section1/card-parts.dart';
+import '../pages/Section1/card-view.dart';
+import '../pages/Section1/menu.dart';
+import '../pages/Section1/playicard.dart';
+import '../pages/Subscriptions page/subscriptions.dart';
+import '../pages/creating_profile_for_kids/create_profile_forkids.dart';
+import '../pages/creating_profile_for_kids/manage_kids.dart';
+import '../pages/favorites/favorites.dart';
+import '../pages/lets-begin.dart';
 import '../pages/login_screen.dart';
+import '../pages/otp-screen.dart';
+import '../pages/recording_voice/recording_voice.dart';
+import '../pages/recording_voice/save_voice.dart';
+import '../pages/recording_voice/start_recording_voice.dart';
 import '../pages/splash_screen.dart';
 import '../pages/verify_email.dart';
 import 'app_routes.dart';
@@ -14,5 +28,20 @@ class AppRoutes {
     GetPage(name: Routes.profile, page: () => ProfileScreen()),
     GetPage(name: Routes.SignUpScreen, page: () => SignUpScreen()),
     GetPage(name: Routes.verifyEmail, page: () => VerifyEmail()),
+    GetPage(name: Routes.StartRecordingVoice, page: () => StartRecordingVoice()),
+    GetPage(name: Routes.HomeScreen, page: () => HomeScreen()),
+    GetPage(name: Routes.SelectedCardView, page: () => SelectedCardView()),
+    GetPage(name: Routes.CreateProfileForkids, page: () => CreateProfileForkids()),
+    GetPage(name: Routes.ManageKids, page: () => ManageKids()),
+    GetPage(name: Routes.RecordingVoice, page: () => RecordingVoice()),
+    GetPage(name: Routes.SaveVoice, page: () => SaveVoice()),
+    GetPage(name: Routes.ViewCardParts, page: () => ViewCardParts()),
+    GetPage(name: Routes.PlayStory, page: () => PlayStory()),
+    GetPage(name: Routes.Favorites, page: () => Favorites()),
+    GetPage(name: Routes.Subscribepage, page: () => Subscribepage()),
+    GetPage(name: Routes.RemindersPage, page: () => RemindersPage()),
+    GetPage(name: Routes.OtpScreen, page: () => OtpScreen()),
+    GetPage(name: Routes.Passwordchange, page: () => Passwordchange()),
+    GetPage(name: Routes.LetsBegin, page: () => LetsBegin()),
   ];
 }
--- lib/app_routes/router.dart
+++ lib/app_routes/router.dart
@@ -69,7 +69,7 @@ final GoRouter appRouter = GoRouter(
 
       path: '/homepage',
       pageBuilder: (context, state) {
-        return buildSlideTransitionPage(ProfileScreen(), state);
+        return buildSlideTransitionPage(HomeScreen(), state);
       },
     ),
     GoRoute(
